f834a34ab9e6f842e96961cff6e5fbc86869439f
Brian Cavalier <brian@briancavalier.com>
2025-01-23 17:10:17 -0500

    counter-notify also deploys counter stack to emphasize that views are modular

---
+++ examples/counter-notify/stack.ts
@@ -0,0 +1,76 @@
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb'
+import { EventBus } from 'aws-cdk-lib/aws-events'
+import { ApplicationLogLevel, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+import { resolve } from 'node:path'
+import { EventBusNotifier, EventBusSubscription, IEventStore } from '../../src/aws-cdk'
+import { commonFunctionEnv, commonFunctionProps } from '../aws-defaults'
+
+export interface CounterNotifyStackProps extends StackProps {
+  eventStore: IEventStore
+}
+
+export class CounterNotifyStack extends Stack {
+  constructor(scope: Construct, id: string, props: CounterNotifyStackProps) {
+    super(scope, id, props)
+
+    const eventBus = new EventBus(this, 'all-events')
+
+    new EventBusNotifier(this, 'counter-events-notifier', {
+      eventStore: props.eventStore,
+      eventBus,
+      applicationLogLevelV2: ApplicationLogLevel.DEBUG,
+    })
+
+
+    // -------------------------------------------
+    // View
+
+    const counterView = new Table(this, 'counter-view', {
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    const update = new NodejsFunction(this, `counter-update-handler`, {
+      ...commonFunctionProps,
+      entry: resolve(__dirname, 'update.ts'),
+      environment: {
+        ...commonFunctionEnv,
+        eventStoreConfig: props.eventStore.config,
+        viewTable: counterView.tableName
+      }
+    })
+
+    counterView.grantReadWriteData(update)
+    props.eventStore.grantReadEvents(update)
+
+    new EventBusSubscription(this, 'counter-events-subscription', {
+      eventBus,
+      eventStore: props.eventStore,
+      subscriber: update,
+    })
+
+    // -------------------------------------------
+    // Query
+
+    const query = new NodejsFunction(this, `counter-query-handler`, {
+      ...commonFunctionProps,
+      entry: resolve(__dirname, 'query.ts'),
+      environment: {
+        ...commonFunctionEnv,
+        viewTable: counterView.tableName
+      }
+    })
+
+    counterView.grantReadData(query)
+
+    const queryUrl = query.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    })
+
+    new CfnOutput(this, 'queryUrl', { value: queryUrl.url })
+  }
+}
--- examples/counter/index.ts
+++ examples/counter/index.ts
@@ -1,64 +1,7 @@
-import { App, CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib'
-import { BillingMode } from 'aws-cdk-lib/aws-dynamodb'
-import { FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda'
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
-import { EventStore } from '../../src/aws-cdk'
-import { commonFunctionEnv, commonFunctionProps } from '../aws-defaults'
+import { App } from 'aws-cdk-lib'
+import { CounterStack } from './stack'
 
 const app = new App()
-const stack = new Stack(app, 'counter')
-
-// -------------------------------------------
-// Event store
-
-export const counterEventStore = EventStore.createTables(stack, 'counter-events', {
-  removalPolicy: RemovalPolicy.DESTROY,
-  billingMode: BillingMode.PAY_PER_REQUEST
-})
-
-// -------------------------------------------
-// Aggregate
-
-const command = new NodejsFunction(stack, `counter-command-handler`, {
-  ...commonFunctionProps,
-  entry: 'command.ts',
-  environment: {
-    ...commonFunctionEnv,
-    eventStoreConfig: counterEventStore.config
-  }
-})
-
-counterEventStore.grantReadWriteEvents(command)
-
-const commandUrl = command.addFunctionUrl({
-  authType: FunctionUrlAuthType.NONE,
-})
-
-// -------------------------------------------
-// Query
-
-const query = new NodejsFunction(stack, `counter-query-handler`, {
-  ...commonFunctionProps,
-  entry: 'query.ts',
-  environment: {
-    ...commonFunctionEnv,
-    eventStoreConfig: counterEventStore.config
-  }
-})
-
-counterEventStore.grantReadEvents(query)
-
-const queryUrl = query.addFunctionUrl({
-  authType: FunctionUrlAuthType.NONE,
-})
-
-// -------------------------------------------
-// Outputs
-
-new CfnOutput(stack, 'commandUrl', { value: commandUrl.url })
-new CfnOutput(stack, 'queryUrl', { value: queryUrl.url })
-
-// -------------------------------------------
-// Done
+new CounterStack(app, 'counter')
 
 app.synth()
