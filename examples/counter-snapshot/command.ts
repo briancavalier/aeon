e71c6fa2b13cc295b4c9bb711e11a594526681f4
Brian Cavalier <brian@briancavalier.com>
2025-01-24 15:13:49 -0500

    Add comments

--- examples/counter-snapshot/command.ts
+++ examples/counter-snapshot/command.ts
@@ -12,21 +12,29 @@ const client = new DynamoDBClient({})
 const store = fromConfigString(process.env.eventStoreConfig, client)
 const snapshotStore = fromConfigString(process.env.snapshotStoreConfig, client)
 
+// Record an updated snapshot every 10 events. In a real application, this number
+// may be higher, tuned to the application needs.
 const maxEventsBetweenSnapshots = 10
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   // In a real app, we'd parse+validate the incoming command
   const command = JSON.parse(event.body ?? '') as CounterCommand
 
+  // Read the latest snapshot, if at least exists, from
+  // the snapshot store
   const snapshot = await readKeyLatest<CounterSnapshot>(snapshotStore, command.key)
 
+  // Compute the range of events we need to read
+  // If the snapshot exists, start reading from its revision
+  // Otherwise, start from the beginning of the event store
   const range = snapshotRange(snapshot?.data)
   console.debug({ snapshot, range })
 
-  // Read the counter's event history from the event store
   const history = readKey<CounterEvent>(store, command.key, range)
 
-  // Rebuild the counter's current value from the event history
+  // Rebuild the counter's current value
+  // If we have a snapshot, start from its value
+  // Otherwise, start from the initial value
   let value = snapshot?.data.value ?? initialValue
   let eventCount = 0
   for await (const event of history) {
@@ -47,6 +55,9 @@ export const handler = async (event: APIGatewayProxyEvent) => {
   // This returns the commit position of the last event appended
   const position = await append(store, events.map(data => ({ ...data, timestamp, data })))
 
+  // If we wrote some new events (position !== undefined), and
+  // it's time to take a new snapshot, compute the latest
+  // snapshot value and append it to the snapshot store.
   if (position && eventCount >= maxEventsBetweenSnapshots) {
     const newValue = events.reduce(update, value)
     console.debug({ msg: 'Adding new snapshot', newValue, timestamp, position })
