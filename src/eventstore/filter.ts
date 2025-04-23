85586a393f2c37f3c61332c60c7e4856da5e0c80
Brian Cavalier <brian@briancavalier.com>
2025-04-22 21:29:24 -0400

    Require at least 2 filters for and/or

--- src/eventstore/filter.ts
+++ src/eventstore/filter.ts
@@ -2,8 +2,7 @@ export type Filter<A> =
   | { readonly _type: 'true' }
   | { readonly _type: 'exists' }
   | { readonly _type: Operator; readonly value: A }
-  | { readonly _type: 'and'; readonly filters: readonly Filter<A>[] }
-  | { readonly _type: 'or'; readonly filters: readonly Filter<A>[] }
+  | { readonly _type: 'and' | 'or'; readonly filters: readonly [Filter<A>, Filter<A>, ...readonly Filter<A>[]] }
   | { readonly [K in string]: Filter<A> }
 
 type Operator = 'prefix' | '=' | '>' | '>=' | '<' | '<=' | '<>'
@@ -21,6 +20,6 @@ export const gte = <A>(value: A): Filter<A> => ({ _type: '>=', value })
 export const lt = <A>(value: A): Filter<A> => ({ _type: '<', value })
 export const lte = <A>(value: A): Filter<A> => ({ _type: '<=', value })
 export const ne = <A>(value: A): Filter<A> => ({ _type: '<>', value })
-export const and = <A>(...filters: readonly Filter<A>[]): Filter<A> => ({ _type: 'and', filters })
-export const or = <A>(...filters: readonly Filter<A>[]): Filter<A> => ({ _type: 'or', filters })
+export const and = <A>(...filters: readonly [Filter<A>, Filter<A>, ...readonly Filter<A>[]]): Filter<A> => ({ _type: 'and', filters })
+export const or = <A>(...filters: readonly [Filter<A>, Filter<A>, ...readonly Filter<A>[]]): Filter<A> => ({ _type: 'or', filters })
 
